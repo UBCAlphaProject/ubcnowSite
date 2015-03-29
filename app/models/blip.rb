eaad20000a6aafbbe0e420ffa7fe627965e8e8f3
Arie Milner <amilner42@gmail.com>
2015-03-29 13:25:33 -0700

    Created Blip class, it is not extending active record and will NOT be refelected in a database

---
+++ app/models/blip.rb
@@ -0,0 +1,16 @@
+class Blip
+
+  def initialize(gid, title, summary, link, time, address, lat, lng)  
+    # Instance variables  
+    @gid = gid  
+    @title = title
+    @summary = summary
+    @link = link
+    @time  = time 
+    @address = address
+    @lat = lat
+    @lng = lng     
+  end  
+  
+
+end
