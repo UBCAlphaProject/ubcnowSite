21764b955abd3158c7dfeaa4c9b0490b69a9e5d6
Arie Milner <amilner42@gmail.com>
2015-04-03 12:17:07 -0600

    Changed form_tag to form_for, making code much cleaner and allowing for edit/update functionality

--- app/controllers/blips_controller.rb
+++ app/controllers/blips_controller.rb
@@ -3,32 +3,20 @@ class BlipsController < ApplicationController
     @blip = Blip.new
   end
 
-  # Make a hash with all the values from the form, convert it to json,
-  # then send the post request, and lastly redirect to index to see all
-  # the blips 
   def create
-    newBlip = {}
-    # TODO GID will be taken from cookies hash and added here to newBlip
-    # Need to recieve the GID to be able to implement/test this, in the 
-    # meantime using the value 0 for all group IDs 
-    newBlip[:gid]     = 1
-    newBlip[:title]   = params[:title]
-    newBlip[:summary] = params[:summary]
-    newBlip[:link]    = params[:link]
-    newBlip[:time]    = params[:time]
-    newBlip[:lat]     = params[:lat].to_i
-    newBlip[:lng]     = params[:lng].to_i
-    jsonBlip = newBlip.to_json
+    newBlip = Blip.new(params[:blip])
+    newBlip.gid = 1                 # TODO change from one to cookies[:gid]
+    newBlip.lat = newBlip.lat.to_i  # Get rid of quotes
+    newBlip.lng = newBlip.lng.to_i  # Get rid of quotes
+    jsonBlip = newBlip.to_json      # Convert to JSON
 
+    # Make post request to server
     c = Curl::Easy.http_post("localhost:9000/api/v1/blip", jsonBlip   
     ) do |curl|
       curl.headers['Accept'] = 'application/json'
       curl.headers['Content-Type'] = 'application/json'
       curl.headers['Api-Version'] = '2.2'
     end
-
-    # DEBUG
-    puts "#{c.body_str}"
 
     redirect_to(:action => index)
   end
--- app/models/blip.rb
+++ app/models/blip.rb
@@ -1,16 +1,22 @@
 class Blip 
- 
-  def initialize(gid = nil, title = nil, summary = nil, link = nil, time = nil, address = nil, lat = nil, lng = nil)  
-    # Instance variables  
-    @gid = gid  
-    @title = title
-    @summary = summary
-    @link = link
-    @time  = time 
-    @address = address
-    @lat = lat
-    @lng = lng     
-  end  
+  # These includes/extends are needed for form_for to work 
+  include ActiveModel::Validations
+  include ActiveModel::Conversion
+  extend ActiveModel::Naming
+
+  attr_accessor :gid, :title, :summary, :link, :time, :address, :lat, :lng
+
+  # Constructor which will take in a hash and set all the respective 
+  # attributes 
+  def initialize(attributes = {})
+    attributes.each do |name, value|
+      send("#{name}=", value)
+    end
+  end
   
+  # Method needed for form_for to work 
+  def persisted?
+    false
+  end
 
 end
