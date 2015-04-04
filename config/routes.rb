cf046ef522cdf1fb45e7a4d6955aa4253c70fbe0
Arie Milner <amilner42@gmail.com>
2015-04-04 13:32:46 -0600

    Got rid of main controller, routed website to blip index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :blips
 
   # Root site to the home page
-  root 'main#home'
+  root 'blips#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
