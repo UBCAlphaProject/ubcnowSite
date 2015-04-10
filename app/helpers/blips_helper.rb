4e4817ae3b426d62f032ec1aeb707d8dc0a5b25a
Arie Milner <amilner42@gmail.com>
2015-04-10 13:06:16 -0600

    Got time on index page in Human Readable Format

--- app/helpers/blips_helper.rb
+++ app/helpers/blips_helper.rb
@@ -1,2 +1,9 @@
 module BlipsHelper
+
+  def timeHumanReadable(epochInt)
+  	return nil if epochInt == nil 							# Time.at cant handle nil value so return nil if int is nil
+    epochNoMili = epochInt / 1000							# Get rid of miliseconds for ease of use with ruby Time class
+    return Time.at(epochNoMili).asctime						# asctime formats a nice string for you
+  end
+
 end
